66fa4d4ea048a7d3edfc81f0237f1dc44bb64b30
Frank Mathy <frankmathy@gmail.com>
2021-01-02 00:10:00 +0100

    Examples for Buttons and Checkboxes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true);
+  return (
+    <div>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={checked}
+            onChange={e => setChecked(e.target.checked)}
+            inputProps={{
+              'aria-label': 'secondary checkbox'
+            }}
+          />
+        }
+        label="Testing Checkbox"
+      />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CheckboxExample />
+        <ButtonGroup variant="contained" color="primary">
+          <Button startIcon={<SaveIcon />} size="large">
+            Save
+          </Button>{' '}
+          <Button startIcon={<DeleteIcon />} size="large" color="secondary">
+            Discard
+          </Button>
+        </ButtonGroup>
       </header>
     </div>
   );
